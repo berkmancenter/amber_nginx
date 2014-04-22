ad46eb52ff5aabffa68cbe97cf026722645e17dd
Jeffrey Licht <jeffrey.licht@gmail.com>
2014-04-22 11:52:00 -0400

    Remove dependency on jQuery

--- js/cayl.js
+++ js/cayl.js
@@ -29,7 +29,16 @@ var cayl = {
   },
 
   country_specific_behavior_exists : function() {
-    return (jQuery("a[data-cache][data-cayl-behavior*=\\,]").length > 0);
+    return (document.querySelectorAll("a[data-cache][data-cayl-behavior*=\\,]").length > 0);
+  },
+
+  callback : function(json) {
+    try {
+      cayl.country = json.country_code;
+      if (cayl.country) {
+        localStorage.setItem('country',cayl.country);
+      }
+    } catch (e) { /* Not supported */ }
   },
 
   get_country : function() {
@@ -39,14 +48,9 @@ var cayl = {
       }
     } catch (e) { /* Not supported */ }
     if (!cayl.country) {
-      jQuery.getJSON('http://freegeoip.net/json/?callback=?', function(data) {
-        cayl.country = data.country_code;
-        try {
-          if (cayl.country) {
-            localStorage.setItem('country',cayl.country);
-          }
-        } catch (e) { /* Not supported */ }
-      });
+      var script = document.createElement('script');
+      script.src = '//freegeoip.net/json/?callback=cayl.callback';
+      document.getElementsByTagName('head')[0].appendChild(script);
     }
   },
 
@@ -140,12 +144,9 @@ var cayl = {
   show_cache : function(e) {
     var behavior = cayl.parse_behavior(this.getAttribute("data-cayl-behavior"));
     var cache = cayl.parse_cache(this.getAttribute("data-cache"));
-
     if (cayl.execute_action(behavior,"cache") && cache.default) {
       window.location.href = cache.default.cache;
-      return false;
-    } else {
-      return true;
+      e.preventDefault();
     }
   },
 
@@ -167,19 +168,36 @@ var cayl = {
         '{{LINK}}' : this.getAttribute("href")
       }
 
-      jQuery("body").append(cayl.replace_args(cayl.get_text('interstitial_html'), replacements));
+      var caylElement = document.createElement('div');
+      caylElement.innerHTML = cayl.replace_args(cayl.get_text('interstitial_html'), replacements);
+      document.body.appendChild(caylElement.firstChild);
 
       /* Center the window */
-      var left = jQuery(window).width()/2 - jQuery(".cayl-interstitial").width()/2;
-      var top =  jQuery(window).height()/2 - jQuery(".cayl-interstitial").height()/2;
-      jQuery(".cayl-interstitial").css({"left" : left, "top": top});
+      var w = window;
+      var d = document;
+      var el = d.documentElement;
+      var g = d.getElementsByTagName('body')[0];
+      var windowWidth = w.innerWidth || el.clientWidth || g.clientWidth;
+      var windowHeight = w.innerHeight|| el.clientHeight|| g.clientHeight;
+      var interstitial = document.querySelectorAll(".cayl-interstitial")[0];
+
+      var left = windowWidth/2 - interstitial.offsetWidth/2;
+      var top =  windowHeight/2 - interstitial.offsetHeight/2;
+      interstitial.style.left = left + "px";
+      interstitial.style.top = top + "px";
 
       /* Clicking on the overlay or close button closes the window */
-      jQuery(".cayl-overlay, .cayl-close").click(function(e) { jQuery(".cayl-overlay, .cayl-interstitial").remove(); return false; });
-
-      return false;
-    } else {
-      return true;
+      var closeEls = document.querySelectorAll(".cayl-overlay, .cayl-close");
+      for (var i = 0; i < closeEls.length; i++) {
+        cayl.util_addEventListener(closeEls[i],'click',function(e) {
+          var els = document.querySelectorAll(".cayl-overlay, .cayl-interstitial");
+          for (var i = 0; i < els.length; i++) {
+              els[i].parentNode.removeChild(els[i]);
+          }
+          e.preventDefault();
+        });
+      }
+      e.preventDefault();
     }
   },
 
@@ -187,24 +205,35 @@ var cayl = {
     cayl.hovering_on_popup = true;
   },
 
-  end_popup_hover : function (e) {
-    cayl.hovering_on_popup = false;
-    jQuery(".cayl-hover").remove();
+  end_popup_hover_function : function (hover) {
+    // Need to make sure that we're not hovering over one of the child elements.
+    // Return a function that captures the original node's descendants
+    var descendants = hover.querySelectorAll('*');
+    return function (e) {
+      var el = e.toElement || e.relatedTarget;
+      for (var i = 0; i < descendants.length; i++) {
+        if (el == descendants[i]) {
+          return;
+        }
+      }
+      cayl.hovering_on_popup = false;
+      var hover = document.querySelectorAll(".cayl-hover")[0];
+      hover.parentNode.removeChild(hover);
+    }
   },
 
   calculate_hover_position : function (target, status) {
-    var offset = jQuery(target).offset();
-    var result = {};
-    result = {"left" : offset.left - 30, "top" : offset.top - 105}
+    var offset = cayl.util_offset(target);
+    var result = {"left" : offset.left - 30, "top" : offset.top - 105}
     if (cayl.rtl) {
-      result.left = result.left + jQuery(target).width() - jQuery(".cayl-hover").width();
+      var hover = document.querySelectorAll(".cayl-hover")[0];
+      result.left = result.left + target.offsetWidth - hover.offsetWidth;
     }
     return result;
   },
 
   start_link_hover : function (e) {
     var behavior = cayl.parse_behavior(this.getAttribute("data-cayl-behavior"));
-
     if (cayl.execute_action(behavior,"hover")) {
       var cache = cayl.parse_cache(this.getAttribute("data-cache"));
       var args = {
@@ -214,41 +243,105 @@ var cayl = {
         '{{LINK}}' : this.getAttribute("href")
       };
       t = this;
-      if (behavior.default.delay) {
-        var timer = setTimeout(function() {
-          jQuery("body").append(cayl.replace_args(behavior.default.status == "up" ? cayl.get_text('hover_html_up') : cayl.get_text('hover_html_down'), args));
+      var delay = behavior[cayl.country] ? behavior[cayl.country].delay : behavior.default.delay;
+      var timer = setTimeout(function() {
+        var caylElement = document.createElement('div');
+        caylElement.innerHTML = cayl.replace_args(behavior.default.status == "up" ? cayl.get_text('hover_html_up') : cayl.get_text('hover_html_down'), args);
+        document.body.appendChild(caylElement.firstChild);
 
-          /* Position the hover text */
-          var offset = jQuery(t).offset();
-          jQuery(".cayl-hover").css(cayl.calculate_hover_position(t, behavior.default.status));
-          jQuery(".cayl-hover").hover(cayl.start_popup_hover, cayl.end_popup_hover);
-        }, behavior.default.delay * 1000);
-        this.getAttribute("cayl-timer",timer);
-      }
+        /* Position the hover text */
+        var hover = document.querySelectorAll(".cayl-hover")[0];
+        var pos = cayl.calculate_hover_position(t, behavior.default.status);
+        hover.style.left = pos.left + "px";
+        hover.style.top = pos.top + "px";
+        cayl.util_addEventListener(hover, 'mouseover', cayl.start_popup_hover);
+        cayl.util_addEventListener(hover, 'mouseout', cayl.end_popup_hover_function(hover));
+      }, delay * 1000);
+      this.setAttribute("cayl-timer",timer);
     }
   },
 
   end_link_hover : function (e) {
-    clearTimeout(this.getAttribute("cayl-timer"));
+    var behavior = cayl.parse_behavior(this.getAttribute("data-cayl-behavior"));
+    if (cayl.execute_action(behavior,"hover")) {
+      clearTimeout(this.getAttribute("cayl-timer"));
 
-    /* Give them some time, and then check if they've moved over the popup before closing popup */
-    setTimeout(function() {
-      if (!cayl.hovering_on_popup) {
-        jQuery(".cayl-hover").remove();
+      /* Give them some time, and then check if they've moved over the popup before closing popup */
+      setTimeout(function() {
+        if (!cayl.hovering_on_popup) {
+          var hover = document.querySelectorAll(".cayl-hover")[0];
+          if (typeof hover != typeof undefined)
+            hover.parentNode.removeChild(hover);
+        }
+      },100);
+    }
+  },
+
+  /* Utility functions to provide support for IE8+ */
+  util_addEventListener : function (el, eventName, handler) {
+    if (el.addEventListener) {
+      el.addEventListener(eventName, handler);
+    } else {
+      el.attachEvent('on' + eventName, function(){
+        handler.call(el);
+      });
+    }
+  },
+
+  util_forEachElement : function (selector, fn) {
+    var elements = document.querySelectorAll(selector);
+    for (var i = 0; i < elements.length; i++)
+       fn(elements[i], i);
+  },
+
+  util_ready : function (fn) {
+    if (document.addEventListener) {
+      document.addEventListener('DOMContentLoaded', fn);
+    } else {
+      document.attachEvent('onreadystatechange', function() {
+        if (document.readyState === 'interactive')
+          fn();
+      });
+    }
+  },
+
+  util_offset : function(elem) {
+      var box = { top: 0, left: 0 };
+      var doc = elem && elem.ownerDocument;
+      var docElem = doc.documentElement;
+      if (typeof elem.getBoundingClientRect !== typeof undefined ) {
+          box = elem.getBoundingClientRect();
       }
-    },100);
+      var win = (doc != null && doc=== doc.window) ? doc: doc.nodeType === 9 && doc.defaultView;
+      return {
+          top: box.top + win.pageYOffset - docElem.clientTop,
+          left: box.left + win.pageXOffset - docElem.clientLeft
+      };
   }
+
+
 };
 
-jQuery(document).ready(function($) {
-    $("a[data-cache][data-cayl-behavior*=cache]").click(cayl.show_cache);
-    $("a[data-cache][data-cayl-behavior*=popup]").click(cayl.show_interstitial);
-    $("a[data-cache][data-cayl-behavior*=hover]").hover(cayl.start_link_hover, cayl.end_link_hover);
+cayl.util_ready(function($) {
+
+    cayl.util_forEachElement("a[data-cache][data-cayl-behavior*=cache]", function(e, i) {
+      cayl.util_addEventListener(e, 'click', cayl.show_cache);
+    });
+    cayl.util_forEachElement("a[data-cache][data-cayl-behavior*=popup]", function(e, i) {
+      cayl.util_addEventListener(e, 'click', cayl.show_interstitial);
+    });
+    cayl.util_forEachElement("a[data-cache][data-cayl-behavior*=hover]", function(e, i) {
+      cayl.util_addEventListener(e, 'mouseover', cayl.start_link_hover);
+      cayl.util_addEventListener(e, 'mouseout', cayl.end_link_hover);
+    });
 
     if (cayl.country_specific_behavior_exists()) {
       cayl.get_country();
     }
 
     /* Drupal-specific code */
-    // cayl.name = Drupal.settings.cayl.name;
-  });
+    if (typeof Drupal != 'undefined') {
+      cayl.name = Drupal.settings.cayl.name;
+    }
+});
+
