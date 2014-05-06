4ca89a62babab8dd24bd4370474c3c699d7cc2ff
Jeffrey Licht <jeff@podconsulting.com>
2014-05-06 15:04:14 -0400

    Remove compiler warnings

--- cayl_utils.c
+++ cayl_utils.c
@@ -19,14 +19,14 @@
 */
 int cayl_build_attribute(cayl_options_t *options, unsigned char *out, char *location, int status, time_t date)
 {
-    char behavior[CAYL_MAX_BEHAVIOR_STRING];
+    unsigned char behavior[CAYL_MAX_BEHAVIOR_STRING];
     char date_string[CAYL_MAX_DATE_STRING];
 
     int rc = cayl_get_behavior(options, behavior, status);
     if (!rc) {
         struct tm *timeinfo = localtime(&date);
         strftime(date_string,CAYL_MAX_DATE_STRING,"%FT%T%z",timeinfo);
-        snprintf(out,
+        snprintf((char *)out,
                  CAYL_MAX_ATTRIBUTE_STRING,
                  "data-cache='%s %s' data-cayl-behavior='%s' ",
                  location,
@@ -56,16 +56,16 @@ int cayl_get_behavior(cayl_options_t *options, unsigned char *out, int status) {
     if (status == CAYL_STATUS_UP) {
         switch (options->behavior_up) {
             case CAYL_ACTION_HOVER:
-                snprintf(out,
+                snprintf((char *)out,
                          CAYL_MAX_ATTRIBUTE_STRING,
                          "up hover:%d",
                          options->hover_delay_up);
                 break;
             case CAYL_ACTION_POPUP:
-                snprintf(out, CAYL_MAX_ATTRIBUTE_STRING, "up popup");
+                snprintf((char *)out, CAYL_MAX_ATTRIBUTE_STRING, "up popup");
                 break;
             case CAYL_ACTION_CACHE:
-                snprintf(out, CAYL_MAX_ATTRIBUTE_STRING, "up cache");
+                snprintf((char *)out, CAYL_MAX_ATTRIBUTE_STRING, "up cache");
                 break;
             case CAYL_ACTION_NONE:
                 break;
@@ -73,16 +73,16 @@ int cayl_get_behavior(cayl_options_t *options, unsigned char *out, int status) {
     } else if (status == CAYL_STATUS_DOWN) {
         switch (options->behavior_down) {
             case CAYL_ACTION_HOVER:
-                snprintf(out,
+                snprintf((char *)out,
                          CAYL_MAX_ATTRIBUTE_STRING,
                          "down hover:%d",
                          options->hover_delay_down);
                 break;
             case CAYL_ACTION_POPUP:
-                snprintf(out, CAYL_MAX_ATTRIBUTE_STRING, "down popup");
+                snprintf((char *)out, CAYL_MAX_ATTRIBUTE_STRING, "down popup");
                 break;
             case CAYL_ACTION_CACHE:
-                snprintf(out, CAYL_MAX_ATTRIBUTE_STRING, "down cache");
+                snprintf((char *)out, CAYL_MAX_ATTRIBUTE_STRING, "down cache");
                 break;
             case CAYL_ACTION_NONE:
                 break;
